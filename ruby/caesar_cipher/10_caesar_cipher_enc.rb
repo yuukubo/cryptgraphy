c2fa3e0d0e2a208969787e0fa188f78d2c9b14dd
yuukubo <i.yusuke.shk@gmail.com>
2016-03-16 22:14:45 +0000

    run with roop

--- ruby/caesar_cipher/10_caesar_cipher_enc.rb
+++ ruby/caesar_cipher/10_caesar_cipher_enc.rb
@@ -1,6 +1,6 @@
 require "readline"
 
-# almost ok but can't do roop in alph.
+# ok! but maybe i can use "mod" to check 26over or not
 
 key = 0
 plaintext = ""
@@ -12,6 +12,7 @@ str2 = ""
 alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 ctr2 = 0
 str3 = []
+ctr3 = 0
 
 puts
 key = Readline.readline("type a number in 0 to 9. > ")
@@ -27,7 +28,14 @@ puts
 while len - 1 >= ctr1 do
 	str2 = str1[ctr1]
 	while ctr2 <= 25 do
-		str3 << alph[ctr2.to_i + key.to_i] if alph[ctr2] == str2
+#		str3 << alph[ctr2.to_i + key.to_i] if alph[ctr2] == str2
+		if alph[ctr2] == str2
+			ctr3 = ctr2.to_i + key.to_i
+			if 26 <= ctr3
+				ctr3 = ctr3 - 26
+			end
+		str3 << alph[ctr3]
+		end
 		ctr2 += 1
 	end
 	ctr2 = 0
@@ -41,3 +49,4 @@ puts ciphertext
 puts
 Readline.readline("Enter to quit! ")
 puts 'See you!'
+sleep(2)

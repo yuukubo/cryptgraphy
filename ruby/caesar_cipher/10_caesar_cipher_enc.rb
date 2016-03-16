28373e267c44b5e334618962ec10f5b18b6aa2e8
yuukubo <i.yusuke.shk@gmail.com>
2016-03-16 22:13:29 +0000

    run but no roop

--- ruby/caesar_cipher/10_caesar_cipher_enc.rb
+++ ruby/caesar_cipher/10_caesar_cipher_enc.rb
@@ -1,22 +1,43 @@
 require "readline"
 
-key = 0
-plaintext = 0
-# ciphertext = 0
+# almost ok but can't do roop in alph.
 
-puts ""
+key = 0
+plaintext = ""
+ciphertext = ""
+str1 = []
+len = 0
+ctr1 = 0
+str2 = ""
+alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+ctr2 = 0
+str3 = []
+
+puts
 key = Readline.readline("type a number in 0 to 9. > ")
 puts 'your key is "' + key + '"'
 
-puts ""
+puts
 plaintext = Readline.readline("type 3 letters in a to z. > ")
 puts 'your plaintext is "' + plaintext + '"'
+str1 = plaintext.split ''
+len = plaintext.length
 
+puts
+while len - 1 >= ctr1 do
+	str2 = str1[ctr1]
+	while ctr2 <= 25 do
+		str3 << alph[ctr2.to_i + key.to_i] if alph[ctr2] == str2
+		ctr2 += 1
+	end
+	ctr2 = 0
+	ctr1 += 1
+end
 
+puts
+ciphertext = 'your ciphertext is "' + str3.join + '"'
+puts ciphertext
 
-
-
-
-puts ""
+puts
 Readline.readline("Enter to quit! ")
 puts 'See you!'
